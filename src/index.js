aeef6c63e9f86fc2941d2fb76b66391b53d00494
Yadija <ayuliadi01@gmail.com>
2022-09-12 00:10:57 +0800

    memasang bootstrap dan mengatur loader css

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-document.writeln("Hello World")
+// import css
+import "./style/css/style.css"
+
+// Import our custom CSS
+import './style/scss/style.scss'
+
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
+
+document.writeln("Hello World");
--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,44 @@ module.exports = {
         path: path.resolve(__dirname, "dist"),
         filename: "bundle.js"
     },
-    mode: "production"
+    mode: "production",
+    module: {
+      rules: [
+        {
+          test: /\.(css)$/,
+          use: [
+            {
+              loader: 'style-loader'
+            },
+            {
+              loader: 'css-loader'
+            }
+          ]
+        },
+        {
+          test: /\.(scss)$/,
+          use: [
+            {
+              loader: 'style-loader'
+            },
+            {
+              loader: 'css-loader'
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                postcssOptions: {
+                  plugins: () => [
+                    require('autoprefixer')
+                  ]
+                }
+              }
+            },
+            {
+              loader: 'sass-loader'
+            }
+          ]
+        }
+      ]
+    }
 }
